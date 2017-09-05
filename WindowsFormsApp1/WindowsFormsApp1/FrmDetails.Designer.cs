f7e50d83b8684ccffbe40ec0e7e7c2f0fbeb432c
Giuseppe Cotti <gius.cotti98@gmail.com>
2017-09-05 21:18:00 +0200

    Graphics on FrmDetails are done

--- WindowsFormsApp1/WindowsFormsApp1/FrmDetails.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FrmDetails.Designer.cs
@@ -28,73 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblName = new System.Windows.Forms.Label();
-            this.lblSurname = new System.Windows.Forms.Label();
-            this.lblSpec = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
             this.TxtBox1 = new System.Windows.Forms.TextBox();
             this.TxtBox2 = new System.Windows.Forms.TextBox();
             this.TxtBox3 = new System.Windows.Forms.TextBox();
+            this.TxtBox4 = new System.Windows.Forms.TextBox();
+            this.TxtBox5 = new System.Windows.Forms.TextBox();
+            this.TxtBox6 = new System.Windows.Forms.TextBox();
+            this.TxtBox7 = new System.Windows.Forms.TextBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.BtnSave = new System.Windows.Forms.Button();
+            this.btnDiscard = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblName
+            // lbl1
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(12, 44);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(35, 13);
-            this.lblName.TabIndex = 0;
-            this.lblName.Text = "Nome";
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(12, 23);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Nome";
             // 
-            // lblSurname
+            // lbl2
             // 
-            this.lblSurname.AutoSize = true;
-            this.lblSurname.Location = new System.Drawing.Point(12, 70);
-            this.lblSurname.Name = "lblSurname";
-            this.lblSurname.Size = new System.Drawing.Size(52, 13);
-            this.lblSurname.TabIndex = 1;
-            this.lblSurname.Text = "Cognome";
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(12, 49);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(52, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Cognome";
             // 
-            // lblSpec
+            // lbl3
             // 
-            this.lblSpec.AutoSize = true;
-            this.lblSpec.Location = new System.Drawing.Point(12, 97);
-            this.lblSpec.Name = "lblSpec";
-            this.lblSpec.Size = new System.Drawing.Size(85, 13);
-            this.lblSpec.TabIndex = 2;
-            this.lblSpec.Text = "Specializzazione";
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(12, 76);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(85, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Specializzazione";
             // 
             // TxtBox1
             // 
-            this.TxtBox1.Location = new System.Drawing.Point(148, 41);
+            this.TxtBox1.Location = new System.Drawing.Point(148, 20);
             this.TxtBox1.Name = "TxtBox1";
             this.TxtBox1.Size = new System.Drawing.Size(100, 20);
             this.TxtBox1.TabIndex = 3;
             // 
             // TxtBox2
             // 
-            this.TxtBox2.Location = new System.Drawing.Point(148, 67);
+            this.TxtBox2.Location = new System.Drawing.Point(148, 46);
             this.TxtBox2.Name = "TxtBox2";
             this.TxtBox2.Size = new System.Drawing.Size(100, 20);
             this.TxtBox2.TabIndex = 4;
             // 
             // TxtBox3
             // 
-            this.TxtBox3.Location = new System.Drawing.Point(148, 94);
+            this.TxtBox3.Location = new System.Drawing.Point(148, 73);
             this.TxtBox3.Name = "TxtBox3";
             this.TxtBox3.Size = new System.Drawing.Size(100, 20);
             this.TxtBox3.TabIndex = 5;
+            // 
+            // TxtBox4
+            // 
+            this.TxtBox4.Location = new System.Drawing.Point(148, 100);
+            this.TxtBox4.Name = "TxtBox4";
+            this.TxtBox4.Size = new System.Drawing.Size(100, 20);
+            this.TxtBox4.TabIndex = 6;
+            // 
+            // TxtBox5
+            // 
+            this.TxtBox5.Location = new System.Drawing.Point(148, 127);
+            this.TxtBox5.Name = "TxtBox5";
+            this.TxtBox5.Size = new System.Drawing.Size(100, 20);
+            this.TxtBox5.TabIndex = 7;
+            // 
+            // TxtBox6
+            // 
+            this.TxtBox6.Location = new System.Drawing.Point(148, 154);
+            this.TxtBox6.Name = "TxtBox6";
+            this.TxtBox6.Size = new System.Drawing.Size(100, 20);
+            this.TxtBox6.TabIndex = 8;
+            // 
+            // TxtBox7
+            // 
+            this.TxtBox7.Location = new System.Drawing.Point(148, 181);
+            this.TxtBox7.Name = "TxtBox7";
+            this.TxtBox7.Size = new System.Drawing.Size(100, 20);
+            this.TxtBox7.TabIndex = 9;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(12, 103);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(35, 13);
+            this.lbl4.TabIndex = 10;
+            this.lbl4.Text = "label1";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(12, 130);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(35, 13);
+            this.lbl5.TabIndex = 11;
+            this.lbl5.Text = "label2";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(12, 157);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(35, 13);
+            this.lbl6.TabIndex = 12;
+            this.lbl6.Text = "label3";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(12, 184);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(35, 13);
+            this.lbl7.TabIndex = 13;
+            this.lbl7.Text = "label4";
+            // 
+            // BtnSave
+            // 
+            this.BtnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.BtnSave.Location = new System.Drawing.Point(15, 240);
+            this.BtnSave.Name = "BtnSave";
+            this.BtnSave.Size = new System.Drawing.Size(75, 23);
+            this.BtnSave.TabIndex = 14;
+            this.BtnSave.Text = "Salva";
+            this.BtnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDiscard
+            // 
+            this.btnDiscard.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnDiscard.Location = new System.Drawing.Point(172, 239);
+            this.btnDiscard.Name = "btnDiscard";
+            this.btnDiscard.Size = new System.Drawing.Size(75, 23);
+            this.btnDiscard.TabIndex = 15;
+            this.btnDiscard.Text = "Annulla";
+            this.btnDiscard.UseVisualStyleBackColor = true;
             // 
             // FrmDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 431);
+            this.ClientSize = new System.Drawing.Size(272, 279);
+            this.Controls.Add(this.btnDiscard);
+            this.Controls.Add(this.BtnSave);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.TxtBox7);
+            this.Controls.Add(this.TxtBox6);
+            this.Controls.Add(this.TxtBox5);
+            this.Controls.Add(this.TxtBox4);
             this.Controls.Add(this.TxtBox3);
             this.Controls.Add(this.TxtBox2);
             this.Controls.Add(this.TxtBox1);
-            this.Controls.Add(this.lblSpec);
-            this.Controls.Add(this.lblSurname);
-            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
             this.Name = "FrmDetails";
             this.Text = "FrmOrders";
             this.ResumeLayout(false);
@@ -104,11 +208,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblName;
-        private System.Windows.Forms.Label lblSurname;
-        private System.Windows.Forms.Label lblSpec;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
         private System.Windows.Forms.TextBox TxtBox1;
         private System.Windows.Forms.TextBox TxtBox2;
         private System.Windows.Forms.TextBox TxtBox3;
+        private System.Windows.Forms.TextBox TxtBox4;
+        private System.Windows.Forms.TextBox TxtBox5;
+        private System.Windows.Forms.TextBox TxtBox6;
+        private System.Windows.Forms.TextBox TxtBox7;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Button BtnSave;
+        private System.Windows.Forms.Button btnDiscard;
     }
 }
