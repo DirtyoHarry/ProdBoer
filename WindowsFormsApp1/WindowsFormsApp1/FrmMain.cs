1eaac97c0e913809ca82e4a8476df9b5eeb93844
Giuseppe Cotti <gius.cotti98@gmail.com>
2017-11-19 14:37:51 +0100

    test

--- WindowsFormsApp1/WindowsFormsApp1/FrmMain.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FrmMain.cs
@@ -218,17 +218,7 @@ namespace ProdCycleBoer
         {
             List<string> a = new List<string>();
 
-            a.Add("1");
-            a.Add("2");
-            a.Add("3");
-            a.Add("4");
-            a.Add("5");
-            a.Add("6");
-            a.Add("7");
-
-            a.Add("1");
-
-            Label1.Text = production.EditOrder(a).ToString();
+           
 
             dataGridView1.Rows.Add("13:30", "johnny", "banco vib 1", "banco vibrante", "pozzetto XYZ");
             dataGridView1.Rows.Add("13:30", "marco", "impastatrice", "impastatura", "coperchi ABC");
